b25e7552556bc5f426f7155731a50ae7a44987bd
Divy Gupta <divygupta208@gmail.com>
2024-03-28 15:48:56 +0530

    link next

--- app/about/page.js
+++ app/about/page.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+
+const details = [
+  { id: 1, name: "Yash", role: "Senior Developer" },
+
+  { id: 2, name: "Vaibhav", role: "Backend Developer" },
+
+  { id: 3, name: "Suresh", role: "Frontend Developer" },
+];
+
 const about = () => {
-  return <div>about</div>;
+  return (
+    <ul>
+      <h1 className="text-xl ml-96">Our Developers</h1>
+      {details.map((item) => (
+        <Link href={`/about/${item.id}`}>
+          <li className="mt-10 ml-96">{item.name}</li>
+        </Link>
+      ))}
+    </ul>
+  );
 };
 
 export default about;
